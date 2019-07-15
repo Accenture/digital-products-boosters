fbcfb20ccef466f95dde5fbc5d168c8cc585b81c
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2019-07-15 12:14:03 -0500

    Set up MDX

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Digital Product Boosters</h1>
+        <a href="https://github.com/Accenture/digital-products-boosters">
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
