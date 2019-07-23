88d22cdc8d6a9dfa1bd4f0c91ed3b77a9f1806da
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2019-07-22 21:49:45 -0400

    update links in react-beginner/meeting-1

---
+++ scripts/create-question.js
@@ -0,0 +1,4 @@
+const createQuestion = verb =>
+  `please input booster and meeting slides you would like to ${verb}?\n(i.e. react-beginner/meeting-1)\n>`;
+
+module.exports = createQuestion;
--- scripts/start-slides.js
+++ scripts/start-slides.js
@@ -1,30 +1,22 @@
 const childProcess = require("child_process");
-const fs = require("fs");
-const readline = require("readline");
+const getBoosterMeetingInfo = require("./get-booster-meeting-info");
+const createQuestion = require("./create-question");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const question = createQuestion("start");
+const startSlides = async () => {
+  const boosterMeetingInfo = await getBoosterMeetingInfo(question);
+  if (!boosterMeetingInfo) return;
 
-const question =
-  "please input booster and meeting slides you would like to run (i.e. react-beginner/meeting-1)?\n";
+  const server = childProcess.spawn("yarn", [
+    "mdx-deck",
+    boosterMeetingInfo.deckPath
+  ]);
 
-rl.question(question, answer => {
-  const deckPath = `boosters/${answer}/deck.mdx`;
-  const deckExists = fs.existsSync(deckPath);
+  server.stdout.on("data", data => console.log(data.toString()));
+  server.stderr.on("data", data => console.log(data.toString()));
+  server.on("close", code =>
+    console.log(`child process exited with code ${code}`)
+  );
+};
 
-  if (deckExists) {
-    const server = childProcess.spawn("yarn", ["mdx-deck", deckPath]);
-
-    server.stdout.on("data", data => console.log(data.toString()));
-    server.stderr.on("data", data => console.log(data.toString()));
-    server.on("close", code =>
-      console.log(`child process exited with code ${code}`)
-    );
-  } else {
-    console.log(`deck file not found in ${deckPath}`);
-  }
-
-  rl.close();
-});
+startSlides();
