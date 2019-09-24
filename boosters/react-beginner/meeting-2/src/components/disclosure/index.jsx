a803e1b99531b40246337b2d369cc766c92a83ca
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2019-09-23 21:46:05 -0400

    solve exercise 0

--- boosters/react-beginner/meeting-2/src/components/disclosure/index.jsx
+++ boosters/react-beginner/meeting-2/src/components/disclosure/index.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "./class-names.module.css";
 
-const DisclosureBase = ({ links, name }) => (
+const DisclosureBase = ({ isOpen, links, name, toggle }) => (
   <div className={classNames.root}>
-    <button className={classNames.button}>{name}</button>
-    <div className={classNames.content}>
-      <ul className={classNames.list}>
-        {links.map(({ href, text }) => (
-          <li className={classNames.listItem} key={text}>
-            <a className={classNames.link} href={href}>
-              {text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <button className={classNames.button} onClick={toggle}>
+      {name}
+    </button>
+    {isOpen && (
+      <div className={classNames.content}>
+        <ul className={classNames.list}>
+          {links.map(({ href, text }) => (
+            <li className={classNames.listItem} key={text}>
+              <a className={classNames.link} href={href}>
+                {text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
   </div>
 );
 
 const Disclosure = props => {
-  const baseProps = { ...props };
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(previousState => !previousState);
+
+  const baseProps = { ...props, isOpen, toggle };
   return <DisclosureBase {...baseProps} />;
 };
 
