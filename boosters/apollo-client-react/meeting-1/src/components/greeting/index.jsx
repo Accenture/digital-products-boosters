92055a41cd345f5cc489c6b7bbfde1ee866921a7
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2019-10-21 00:08:00 -0400

    solve exercise 0

--- boosters/apollo-client-react/meeting-1/src/components/greeting/index.jsx
+++ boosters/apollo-client-react/meeting-1/src/components/greeting/index.jsx
@@ -1,5 +1,8 @@
+import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import React from "react";
+import Error from "../error";
+import Loading from "../loading";
 
 export const QUERY_GET_CURRENT_USER_LOGIN = gql`
   {
@@ -14,7 +17,11 @@ export const QUERY_GET_CURRENT_USER_LOGIN = gql`
 const GreetingBase = ({ message }) => <h1>{message}</h1>;
 
 const Greeting = () => {
-  const message = "Hello";
+  const { loading, error, data } = useQuery(QUERY_GET_CURRENT_USER_LOGIN);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+
+  const message = `Hello ${data.viewer.login}`;
 
   const baseProps = { message };
   return <GreetingBase {...baseProps} />;
