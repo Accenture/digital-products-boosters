bd63e160125a2f365098ae8621ce5c0a813ece57
Henry Chao <henry.chao@accenture.com>
2020-01-08 09:45:17 -0500

    Exercise 3 answer

--- boosters/graphql-js/meeting-3/resolvers/query.js
+++ boosters/graphql-js/meeting-3/resolvers/query.js
@@ -1,7 +1,7 @@
 const db = require('../models');
 
 exports.repository = (object, args, context) =>
-  db.repository.findOne({ where: { id: args.id } });
+  db.repository.gen(args.id, context.currentUser);
 
 exports.user = (object, args, context) =>
   db.user.findOne({ where: { id: args.id } });
