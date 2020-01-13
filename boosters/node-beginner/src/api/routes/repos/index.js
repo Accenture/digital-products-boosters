6821c88eba123641ba01ce1445dd0e329aaee12d
Henry Chao <henry.chao@accenture.com>
2020-01-13 10:38:31 -0500

    Refactored directory references

--- boosters/node-beginner/src/api/routes/repos/index.js
+++ boosters/node-beginner/src/api/routes/repos/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const dbRepo = require('../../../models').repo;
+const express = require("express");
+const { Repo: dbRepo } = require("../../../../models");
 const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .get(async (req, res) => {
     const repos = await dbRepo.findAll();
     res.status(200);
@@ -14,18 +14,18 @@ router
       await dbRepo.create({
         name: req.body.name,
         userId: req.body.userId,
-        isPrivate: req.body.isPrivate,
+        isPrivate: req.body.isPrivate
       });
       res.status(201);
-      res.end('Added repo');
+      res.end("Added repo");
     } catch (e) {
       res.status(400);
       console.error(e);
-      res.end('Invalid request');
+      res.end("Invalid request");
     }
   });
 
-router.get('/:repoId', async (req, res) => {
+router.get("/:repoId", async (req, res) => {
   const repo = await dbRepo.findByPk(req.params.repoId);
   res.send(repo);
 });
--- boosters/node-beginner/src/api/routes/users/index.js
+++ boosters/node-beginner/src/api/routes/users/index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const dbUser = require('../../../models').user;
-const dbRepo = require('../../../models').repo;
+const { User: dbUser, Repo: dbRepo } = require("../../../../models");
 
 router
-  .route('/')
+  .route("/")
   .get(async (req, res) => {
     const users = await dbUser.findAll();
     res.send(users);
@@ -13,27 +12,27 @@ router
     try {
       await dbUser.create({
         firstName: req.body.name,
-        lastName: req.body.lastName,
+        lastName: req.body.lastName
       });
       res.status(201);
-      res.end('Added user');
+      res.end("Added user");
     } catch (e) {
       res.status(400);
       console.error(e);
-      res.end('Invalid request');
+      res.end("Invalid request");
     }
   });
 
-router.get('/:userId', async (req, res) => {
+router.get("/:userId", async (req, res) => {
   const user = await dbUser.findByPk(req.params.userId);
   res.send(user);
 });
 
-router.get('/:userId/repos', async (req, res) => {
+router.get("/:userId/repos", async (req, res) => {
   const repos = await dbRepo.findAll({
     where: {
-      userId: req.params.userId,
-    },
+      userId: req.params.userId
+    }
   });
   res.send(repos);
 });
